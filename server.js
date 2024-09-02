64e4d3ac15cb6d2b106a6a7df0ad89a89569f7fe
NuralySol <soltonbekov@gmail.com>
2024-09-01 22:21:01 -0400

    some cors changes to accept everything

--- server.js
+++ server.js
@@ -11,7 +11,13 @@ connectDB();
 
 const app = express();
 
-app.use(cors());
+app.use(cors({
+    origin: '*', 
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'], 
+    allowedHeaders: ['Content-Type', 'Authorization', 'X-Requested-With'],
+    credentials: true, 
+}));
+
 app.use(morgan('combined'));
 app.use(express.json());
 app.use('/', routes);
